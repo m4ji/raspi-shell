04323935002e7bc2ebb272b21d3c7dad6c808619
Maji <79071613+m4ji@users.noreply.github.com>
2021-05-21 08:42:44 -0700

    Update and rename install.sh to stage2.sh

---
+++ stage2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+if [ `whoami` != root ];
+then
+	echo "Run the script as root dumbass"
+	exit
+elif [ `whoami` = root ];
+then
+	cp ~/new-pihole/.bashrc ~/.bashrc
+	cp ~/new-pihole/.aliasrc ~
+	cp ~/new-pihole/default-dns ~
+	cp ~/new-pihole/startup-dns ~
+	cp ~/new-pihole/pihole-dns ~/etc/init.d/
+	chmod a+x ~/etc/init.d/pihole-dns.sh
+	update-rc.d pihole-dns.sh defaults
+	apt install vim python3-pip
+	pip3 install bpytop --upgrade
+	exit
+fi
