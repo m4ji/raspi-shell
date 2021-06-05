9204f36d1eaade7f773a65198d54e9cef7518998
Maji <79071613+m4ji@users.noreply.github.com>
2021-06-05 12:57:27 -0700

    Delete stage3.sh

--- stage3.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-git clone https://github.com/slicer69/doas ~/doas
-git clone --depth 1 https://github.com/pi-hole/pi-hole.git ~/Pi-hole
-mkdir -p ~/bin
-curl -o ~/bin/tldr https://raw.githubusercontent.com/raylee/tldr/master/tldr
-chmod +x ~/bin/tldr
-exit
