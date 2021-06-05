65f2e21c5e12a06d6db2fc654c9789e02ae87a9d
Maji <79071613+m4ji@users.noreply.github.com>
2021-06-05 10:49:31 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ then
 	exit
 elif [ `whoami` = root ];
 then
-  chmod a+x stage1.sh stage2.sh stage3.sh stage4.sh
-  bash stage1.sh
-  bash stage2.sh
-  bash stage3.sh
-  bash stage4.sh
+	chmod a+x stage1.sh stage2.sh stage3.sh stage4.sh
+	bash stage1.sh
+	bash stage2.sh
+	bash stage3.sh
+	bash stage4.sh
 	exit
 fi
