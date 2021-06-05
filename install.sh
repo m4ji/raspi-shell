22dcc0ba22082208215f14599376b6ff996e727c
Maji <79071613+m4ji@users.noreply.github.com>
2021-06-05 12:58:00 -0700

    Add files via upload

--- install.sh
+++ install.sh
@@ -5,9 +5,26 @@ then
 	exit
 elif [ `whoami` = root ];
 then
-	chmod a+x stage1.sh stage2.sh stage3.sh
-	bash stage1.sh
-	bash stage2.sh
-	bash stage3.sh
+	apt update
+	apt upgrade -y
+	apt install -y build-essential make bison flex libpam0g-dev vim python3-pip
+	cp /home/pi/new-pihole/.bashrc /home/pi/.bashrc
+	cp /home/pi/new-pihole/.aliasrc /home/pi
+	cp /home/pi/new-pihole/default-dns /home/pi
+	cp /home/pi/new-pihole/startup-dns /home/pi
+	cp /home/pi/new-pihole/pihole-dns.sh /home/pi/etc/init.d
+	chmod a+x /home/pi/etc/init.d/pihole-dns.sh
+	update-rc.d pihole-dns.sh defaults
+	pip3 install bpytop --upgrade
+	git clone https://github.com/slicer69/doas /home/pi/doas
+	make --directory /home/pi/doas
+	make --directory /home/pi/doas install
+	git clone --depth 1 https://github.com/pi-hole/pi-hole.git /home/pi/Pi-hole
+	mkdir -p /home/pi/bin
+	curl -o /home/pi/bin/tldr https://raw.githubusercontent.com/raylee/tldr/master/tldr
+	chmod +x /home/pi/bin/tldr
+	source /home/pi/.bashrc
+	source /home/pi/.aliasrc
+	bash /home/pi/Pi-hole/automated\ install/basic-install.sh
 	exit
 fi
